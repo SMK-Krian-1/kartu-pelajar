d065391a6b6db4ae098eca5dba0ce1b22c09d941
Brian <mail@standinshd.site>
2020-07-25 13:24:11 +0700

    updated
    * mysql as db
    * form input page

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ const bodyParser = require("body-parser");
 const hbs = require("hbs");
 const fs = require("fs");
 const path = require("path");
+const sql = require("mysql");
 const upload = multer({ dest: "uploads/" });
 const app = express();
+const db = sql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "data_kartu"
+});
+db.connect(err => {
+  if (err) return console.error(`MySQL connection failed : ${err}`);
+  console.log(`MySQL successfully connected!`);
+});
 
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "hbs");
@@ -36,22 +47,38 @@ app.post("/post-data", upload.single("fileFoto"), (req, res) => {
   res.render("kartu", { data, foto });
 });
 
-app.post("/:jurusan/add", (req, res) => {
-  const jurusan = req.params;
-  res.json(jurusan)
+app.get("/:jurusan", (req, res) => {
+  res.json("hai");
+});
+
+app.get("/form/:jurusan", (req, res) => {
+  const { jurusan } = req.params;
+  res.render("form", { jurusan });
+});
+
+app.post("/form/:jurusan/add", (req, res) => {
+  const { jurusan } = req.params;
+  const { namaLengkap, NIS, NIS2, NISN, komp_keahlian, tempatLahir, tglLahir, blnLahir, thnLahir, alamat } = req.body;
+  console.log(req.body)
+  const ttl = `${tempatLahir}, ${tglLahir} ${blnLahir} ${thnLahir}`;
+  db.query(`INSERT INTO siswa(kelas, nama, nis, absen, nisn, komp_keahlian, ttl, alamat) VALUES('${jurusan}', '${namaLengkap}', '${NIS}', '${NIS2}', '${NISN}', '${komp_keahlian}', '${ttl}', '${alamat}')`, (err, result, field) => {
+    if (err) throw err;
+    console.log(result);
+    console.log(`new field : ${field}`);
+  });
 });
 
 app.post("/:jurusan/get/:id", (req, res) => {
-  const jurusan = req.params;
-  res.json(jurusan)
+  const { jurusan } = req.params;
+  
 });
 
-app.post("/:jurusan/upd/:id", (req, res) => {
-  const jurusan = req.params;
-  res.json(jurusan)
+app.post("form/:jurusan/upd/:id", (req, res) => {
+  const { jurusan } = req.params;
+  
 });
 
-app.post("/:jurusan/del/:id", (req, res) => {
+app.post("form/:jurusan/del/:id", (req, res) => {
   const jurusan = req.params;
   res.json(jurusan)
 });
